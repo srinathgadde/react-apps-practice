48113a4a50674e8a8dccf6acc1a9bd8efc919673
srinathgadde <srinathgaddde@gmail.com>
2024-01-21 12:17:04 +0530

    custom react elements added

---
+++ src/Sky.jsx
@@ -0,0 +1,9 @@
+function Sky() {
+  return (
+    <div>
+      <h2>Sky is Blue</h2>
+      <p>Test para</p>
+    </div>
+  );
+}
+export default Sky;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+// import App from "./App";
+import Sky from "./Sky";
 
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    visit google
+  </a>
+);
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click to visit Google"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  reactElement
+  // </React.StrictMode>
 );
